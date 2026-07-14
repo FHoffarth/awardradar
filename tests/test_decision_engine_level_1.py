91286497a1e12b912221142bc2b81d53efdcc3b8
Florian Hoffarth <florian.hoffarth1981@gmail.com>
2026-07-14 19:08:15 +0200

    style: finalize premium landing and public shell

--- tests/test_decision_engine_level_1.py
+++ tests/test_decision_engine_level_1.py
@@ -561,8 +561,8 @@ class AboutMethodologyPage(unittest.TestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
         html = response.get_data(as_text=True)
-        self.assertIn("index-Dd_o58S1.js", html)
-        self.assertIn("index-BTIf-htL.css", html)
+        self.assertIn("index-DMpDrovT.js", html)
+        self.assertIn("index-BA2Mktow.css", html)
 
     def test_new_app_route_serves_google_bundle(self):
         response = self.client.get("/app")
