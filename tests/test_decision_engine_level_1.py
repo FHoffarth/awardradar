5674d4b922739f864317ee3a5426554c2aca4aa5
Florian Hoffarth <florian.hoffarth1981@gmail.com>
2026-07-14 17:34:13 +0200

    test: update landing asset hashes after visual polish build

--- tests/test_decision_engine_level_1.py
+++ tests/test_decision_engine_level_1.py
@@ -561,8 +561,8 @@ class AboutMethodologyPage(unittest.TestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
         html = response.get_data(as_text=True)
-        self.assertIn("index-kWjaLhcZ.js", html)
-        self.assertIn("index-BbhzMao4.css", html)
+        self.assertIn("index-CanO7lJv.js", html)
+        self.assertIn("index-Dfri9__h.css", html)
 
     def test_new_app_route_serves_google_bundle(self):
         response = self.client.get("/app")
